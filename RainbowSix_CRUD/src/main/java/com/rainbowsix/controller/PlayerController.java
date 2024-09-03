6c408dccf7d4fa1db62db2607b33d96b59196bc1
Ítalo Santana <italoeugenio539@gmail.com>
2024-09-03 16:22:48 -0300

    controller

--- RainbowSix_CRUD/src/main/java/com/rainbowsix/controller/PlayerController.java
+++ RainbowSix_CRUD/src/main/java/com/rainbowsix/controller/PlayerController.java
@@ -1,4 +1,15 @@
 package main.java.com.rainbowsix.controller;
 
+import main.java.com.rainbowsix.model.Player;
+
 public class PlayerController {
+    PlayerController PlayerController = new PlayerController();
+
+    public void criarJogador(){
+
+    }
+
+    public Player informacoesDoPlayer(int id){
+       return PlayerController.informacoesDoPlayer(id);
+    }
 }
