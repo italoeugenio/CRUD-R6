b7c9f3c92207510b394f67046c21bcbb6bffad5e
Ítalo Santana <italoeugenio539@gmail.com>
2024-10-29 13:54:17 -0300

    update playerController

--- RainbowSix_CRUD/src/main/java/com/rainbowsix/controller/PlayerController.java
+++ RainbowSix_CRUD/src/main/java/com/rainbowsix/controller/PlayerController.java
@@ -16,5 +16,31 @@ public class PlayerController {
         playerList.add(player);
     }
 
-    
+    public List<Player> getPlayerList(){
+        return playerList;
+    }
+
+    public Player getPlayerById(int id) {
+        int high = playerList.size() - 1;
+        int low = 0;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            Player midPlayer = playerList.get(mid);
+
+            if (midPlayer.getId() == id) {
+                return midPlayer;
+            } else if (midPlayer.getId() < id) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return null;
+    }
+
+    public void updateBock(int id, String nickname, String playerName, Role role, String nationality, int age, Team currentTeam){
+        if()
+
+    }
 }
