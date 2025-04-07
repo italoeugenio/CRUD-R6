5c6bafd91d14e49ebdf9c929aee5c3a7f687db05
Ítalo Santana <112109465+italoeugenio@users.noreply.github.com>
2025-04-07 15:49:02 -0300

    Delete RainbowSix_CRUD directory

--- RainbowSix_CRUD/src/main/java/com/rainbowsix/application/Main.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.rainbowsix.application;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
--- RainbowSix_CRUD/src/main/java/com/rainbowsix/controller/TeamsController.java
+++
@@ -1,4 +0,0 @@
-package main.java.com.rainbowsix.controller;
-
-public class TeamsController {
-}
